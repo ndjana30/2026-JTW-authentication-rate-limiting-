4982ee3fc5d8dd51e1abacb2cd6255e924b5b3bc
ndjana30 <davyndjana@gmail.com>
2026-09-08 11:53:48 +0100

    connected to online database

--- src/main/java/com/ndjana/rate/models/ProfessionalGenre.java
+++ src/main/java/com/ndjana/rate/models/ProfessionalGenre.java
@@ -1,13 +1,17 @@
 package com.ndjana.rate.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "ProfessionalGenre")
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProfessionalGenre {
 
     @Id
